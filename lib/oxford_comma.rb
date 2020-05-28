2094da3fc664aae7fb440b29a2fe3b7788c62802
candescent-torrent-9701 <williamsmatthew333@gmail.com>
2020-05-28 03:32:41 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,21 @@
 def oxford_comma(array)
-
+@array = array
+@new_array = []
+if @array.length < 2
+@array.join
+elsif @array.length < 3
+@array.join(" and ")
+else @array.length < 4
+@array.insert(-2, "and")
+@array.map do |value|
+@new_array << value
+@new_array << ","
+@new_array << " "
+end
+@new_array.pop
+@new_array.pop
+@new_array.delete_at(-3)
+@new_array.to_s
+@new_array.join
+end
 end
