77ce8174ae63eb0f520d89534eeab3e5591d2f37
candescent-torrent-9701 <williamsmatthew333@gmail.com>
2020-05-29 16:04:30 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -5,6 +5,7 @@ if @array.length < 2
 @array.join
 elsif @array.length < 3
 @array.join(" and ")
+<<<<<<< HEAD
 else @array.length < 4
 @array.insert(-2, "and")
 @array.map do |value|
@@ -17,5 +18,15 @@ end
 @new_array.delete_at(-3)
 @new_array.to_s
 @new_array.join
+=======
+elsif @array.length < 4
+@array.insert(-2, "and")
+@array.map do |value|
+@array << value
+@array << ","
+end
+@array.to_s
+@array.join(" ")
+>>>>>>> a62234808346783131e03d4dd35554d63ccf8d80
 end
 end
